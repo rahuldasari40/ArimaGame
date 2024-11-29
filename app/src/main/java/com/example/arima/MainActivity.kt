c5cd7412d6cf552d2d15e4fe2ab8db7e5cec3bfd
rahuldasari40 <rahuldasari40@gmail.com>
2024-11-29 19:03:44 +0530

    Commit 5

--- app/src/main/java/com/example/arima/MainActivity.kt
+++ app/src/main/java/com/example/arima/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.arima.ui.theme.ArimaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +17,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArimaTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-    }
-}
-}
+                    Box(
+            modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+                }
+            }
         }
     }
+}
 @Composable
     Box(
-    }
-}
+            }
+            }
